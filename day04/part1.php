5064ba79d138a119fdea378b20254b656269ae23
Grégory Marigot <gmarigot@teicee.com>
2023-12-04 22:52:57 +0100

    Jour 4

---
+++ day04/part1.php
@@ -0,0 +1,21 @@
+<?php
+$inputs = file('input.txt');
+
+
+function count_points($win, $our) {
+	$nb = 0;
+	foreach ($our as $n) {
+		if (in_array($n, $win)) $nb++;
+	}
+	return ($nb) ? 2 ** ($nb - 1) : 0;
+}
+
+
+$sum = 0;
+foreach ($inputs as $line) {
+	if (! preg_match('/^Card *\d+:([\d\s]+)\|([\d\s]+)$/', $line, $m)) die($line);
+	$win = preg_split('/\s+/', trim($m[1]));
+	$our = preg_split('/\s+/', trim($m[2]));
+	$sum+= count_points($win, $our);
+}
+printf("SUM=%d\n", $sum);
---
+++ day04/part2.php
@@ -0,0 +1,33 @@
+<?php
+$inputs = file('input.txt');
+
+
+function count_matchs($win, $our) {
+	$nb = 0;
+	foreach ($our as $n) {
+		if (in_array($n, $win)) $nb++;
+	}
+	return $nb;
+}
+
+
+$sum = 0;
+$qty = array();
+foreach ($inputs as $line) {
+	if (! preg_match('/^Card *(\d+):([\d\s]+)\|([\d\s]+)$/', $line, $m)) die($line);
+	$num = $m[1];
+	$win = preg_split('/\s+/', trim($m[2]));
+	$our = preg_split('/\s+/', trim($m[3]));
+	
+	$nb = count_matchs($win, $our);
+	
+	if (! isset($qty[ $num ])) $qty[ $num ] = 0;
+	$qty[ $num ]++;
+	
+	for ($n = 1; $n <= $nb; $n++) {
+		if (! isset($qty[ $num + $n ])) $qty[ $num + $n ] = 0;
+		$qty[ $num + $n ]+= $qty[ $num ];
+	}
+	$sum+= $qty[ $num ];
+}
+printf("SUM=%d\n", $sum);
